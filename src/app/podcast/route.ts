c60537bd7d999063d8fcfff447b64ab7d2e12648
Uzer <uzersayed9@gmail.com>
2025-05-13 23:43:37 +0530

    fix the route of podcast

--- src/app/podcast/route.ts
+++ src/app/podcast/route.ts
@@ -9,8 +9,8 @@ export async function GET() {
     // Find the latest active season
     const latestSeason = podcastSeasons.find((season: PodcastSeason) => season);
     if (latestSeason) {
-        return NextResponse.redirect(new URL(`/podcast/${latestSeason.id}`, process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8082'));
+        return NextResponse.redirect(new URL(`/podcast/${latestSeason.id}`, process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8082'));
     }
     // Fallback to first season if no active season is found
-    return NextResponse.redirect(new URL(`/podcast/${podcastSeasons[0].id}`, process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8082'));
+    return NextResponse.redirect(new URL(`/podcast/${podcastSeasons[0].id}`, process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8082'));
 } 
